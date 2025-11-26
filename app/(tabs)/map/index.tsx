b8c2f5d9a1997cc0804aa3317f011ec57f05270a
kassiano <kassiano@weedvision.com.br>
2025-11-25 23:01:52 -0300

    [fate] - add map lib

--- app/(tabs)/map/index.tsx
+++ app/(tabs)/map/index.tsx
@@ -1,44 +1,25 @@
-import { Heading } from '@/components/ui/heading';
 import React from 'react';
 
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { Image } from 'expo-image';
-import { StyleSheet, View } from 'react-native';
+import { Page } from '@/components/shared/Page';
+import { Platform, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 export default function MapPage() {
+  const mapProvider = Platform.OS === 'android' ? PROVIDER_GOOGLE : undefined;
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{
-        light: '#A1CEDC',
-        dark: '#1D3D47',
-      }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <Page needsSafeArea={false} needsPadding={false}>
+      <View className="flex-1">
+        <MapView
+          className="h-full w-full"
+          provider={mapProvider}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
         />
-      }
-    >
-      <View className="d-flex w-52 gap-2">
-        <Heading size={'3xl'}>Map Page</Heading>
       </View>
-    </ParallaxScrollView>
+    </Page>
   );
 }
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
