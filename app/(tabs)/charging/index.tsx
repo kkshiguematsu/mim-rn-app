9865602dc7b75f51ae0951aa11205ae5becc548c
kassiano <kassiano@weedvision.com.br>
2025-11-26 13:18:40 -0300

    [feat] - Update chargingPage dark theme

--- app/(tabs)/charging/index.tsx
+++ app/(tabs)/charging/index.tsx
@@ -9,10 +9,10 @@ export default function ChargingPage() {
   return (
     <Page needsPadding needsSafeArea alignItems="center" justifyContent="center">
       <View className="flex w-[70%] items-center justify-center gap-10">
-        <View className="h-56 w-56 rounded-full bg-primary-200/50 p-14">
+        <View className="h-56 w-56 rounded-full bg-primary-200/50 p-14 dark:bg-primary-900">
           <Icon as={EvCharger} className="h-full w-full text-primary-400" />
         </View>
-        <Heading size="xl" className="flex-wrap text-center text-neutral-700">
+        <Heading size="xl" className="flex-wrap text-center text-neutral-700 dark:text-neutral-300">
           Nenhum carregamento em andamento
         </Heading>
       </View>
