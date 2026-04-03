af899fcb2ac7ff90947b7f83fb9de50721ac5c09
kkshiguematsu <kassiano.e@hotmail.com>
2026-04-03 10:36:40 -0300

    add boostrap auto login

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,21 @@
+import { useUserStore } from '@/hooks/store/useUserStore';
 import { Redirect } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function Index() {
-  return <Redirect href="/(auth)/login" />;
+  const { bootstrap, isLoading, isAuthenticated } = useUserStore();
+
+  useEffect(() => {
+    bootstrap();
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (!isAuthenticated) {
+    return <Redirect href="/(auth)/login" />;
+  }
+
+  return <Redirect href="/(tabs)/home" />;
 }
